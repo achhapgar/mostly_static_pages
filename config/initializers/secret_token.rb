17fffaa9172249cbe65959815b3029a9d5731a32
achhhapar <ahunawar@hotmail.com>
2013-06-24 19:09:45 -0700

    First commit to mostly_static_pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MostlyStaticPages::Application.config.secret_token = 'b96d2d4821925e125a5abda6b8bc8c230b6fd50a8f938a73e82f5f045addd71c0891b9409ab330e483508b3608318a22a7ec917f8180ee38c7b278ca3f547f85'
